05518ac54fb9074ce9e4ae5d5954d1201dd0961d
Alexej Doronin <lyosha.doronin@gmail.com>
2025-01-13 23:57:30 +0200

    Compiles

---
+++ src/radapter_slam.cpp
@@ -0,0 +1,24 @@
+#include "radapter/radapter.hpp"
+
+using namespace radapter;
+
+
+
+class RadapterSlam final : public radapter::Worker {
+    Q_OBJECT
+public:
+    RadapterSlam(QVariantList const& args, Instance* inst) :
+        Worker(inst, "slam")
+    {
+
+    }
+    void OnMsg(QVariant const& msg) override {
+
+    }
+};
+
+
+
+RADAPTER_PLUGIN(RadapterSlam, "radapter.plugins.Slam")
+
+#include "radapter_slam.moc"
